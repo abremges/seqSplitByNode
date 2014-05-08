4dd2fb0c888765ae1af3732c983f0ae4ed8d1685
Andreas Bremges <andreas@cebitec.uni-bielefeld.de>
2014-05-08 11:33:16 +0200

    License date update

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /* The MIT License
 
-   Copyright (c) 2012-2013 Andreas Bremges <abremges@cebitec.uni-bielefeld.de>
+   Copyright (c) 2014 Andreas Bremges <abremges@cebitec.uni-bielefeld.de>
 
    Permission is hereby granted, free of charge, to any person obtaining
    a copy of this software and associated documentation files (the
